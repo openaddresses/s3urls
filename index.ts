36bfce6b3a34aa1b0550411cc2974adc806e1359
ingalls <nick@ingalls.ca>
2023-02-15 20:21:42 -0500

    Migrate to typescript

---
+++ index.ts
@@ -0,0 +1,58 @@
+export interface S3Params {
+    Bucket: string | undefined;
+    Key: string | undefined;
+};
+
+export interface S3Permutations {
+    S3: string;
+    BucketInPath: string;
+    BucketInHost: string;
+};
+
+export default class S3URLs {
+    static fromUrl(url: string): S3Params {
+        const uri = new URL(url);
+        uri.pathname = decodeURIComponent(uri.pathname || '');
+
+        const style = (function(uri) {
+            if (uri.protocol === 's3:') return 'S3';
+            if (/^s3[.-](\w{2}-(gov-)?\w{4,9}-\d\.)?amazonaws\.com/.test(uri.hostname)) return 'BucketInPath';
+            if (/\.s3[.-](\w{2}-(gov-)?\w{4,9}-\d\.)?amazonaws\.com/.test(uri.hostname)) return 'BucketInHost';
+        })(uri);
+
+        let bucket, key;
+        if (style === 'S3') {
+            bucket = uri.hostname;
+            key = uri.pathname.slice(1);
+        } else if (style === 'BucketInPath') {
+            bucket = uri.pathname.split('/')[1];
+            key = uri.pathname.split('/').slice(2).join('/');
+        } else if (style === 'BucketInHost') {
+            const match = uri.hostname.replace(/\.s3[.-](\w{2}-(gov-)?\w{4,9}-\d\.)?amazonaws\.com(\.cn)?/, '');
+            if (match.length) {
+                bucket = match;
+            } else {
+                bucket =  uri.hostname.split('.')[0];
+            }
+            key = uri.pathname.slice(1);
+        }
+
+        return {
+            Bucket: bucket,
+            Key: key
+        };
+    }
+
+    static toUrl(bucket: string, key: string): S3Permutations {
+        return {
+            'S3': ['s3:/', bucket, key].join('/'),
+            'BucketInPath': ['https://s3.amazonaws.com', bucket, key].join('/'),
+            'BucketInHost': ['https:/', bucket + '.s3.amazonaws.com', key].join('/')
+        };
+    }
+
+    static valid(url: string): boolean {
+        const params = this.fromUrl(url);
+        return !!(params.Bucket && params.Key)
+    }
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,9 +3,9 @@ import s3Urls from '../index.js';
 
 test('toUrl', (t) => {
     const result = s3Urls.toUrl('bucket', 'key');
-    t.equal(result.s3, 's3://bucket/key', 'expected s3 url');
-    t.equal(result['bucket-in-path'], 'https://s3.amazonaws.com/bucket/key', 'expected bucket-in-path url');
-    t.equal(result['bucket-in-host'], 'https://bucket.s3.amazonaws.com/key', 'expected bucket-in-host url');
+    t.equal(result.S3, 's3://bucket/key', 'expected s3 url');
+    t.equal(result['BucketInPath'], 'https://s3.amazonaws.com/bucket/key', 'expected BucketInPath url');
+    t.equal(result['BucketInHost'], 'https://bucket.s3.amazonaws.com/key', 'expected BucketInHost url');
     t.end();
 });
 
@@ -44,119 +44,108 @@ test('fromUrl: s3 bucket only style with slash', (t) => {
     t.end();
 });
 
-test('fromUrl: bucket-in-path style', (t) => {
+test('fromUrl: BucketInPath style', (t) => {
     const result = s3Urls.fromUrl('https://s3.amazonaws.com/bucket/the/whole/key');
     t.equal(result.Bucket, 'bucket', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-path style in cn-north-1', (t) => {
+test('fromUrl: BucketInPath style in cn-north-1', (t) => {
     const result = s3Urls.fromUrl('https://s3.cn-north-1.amazonaws.com.cn/bucket/the/whole/key');
     t.equal(result.Bucket, 'bucket', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-path style in cn-north-1 w/ dot bucket', (t) => {
+test('fromUrl: BucketInPath style in cn-north-1 w/ dot bucket', (t) => {
     const result = s3Urls.fromUrl('https://s3.cn-north-1.amazonaws.com.cn/results.openaddresses.io/the/whole/key');
     t.equal(result.Bucket, 'results.openaddresses.io', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-path style in ap-southeast-1', (t) => {
+test('fromUrl: BucketInPath style in ap-southeast-1', (t) => {
     const result = s3Urls.fromUrl('https://s3.ap-southeast-1.amazonaws.com/bucket/the/whole/key');
     t.equal(result.Bucket, 'bucket', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-path dashed in cn-north-1', (t) => {
+test('fromUrl: BucketInPath dashed in cn-north-1', (t) => {
     const result = s3Urls.fromUrl('https://s3-cn-north-1.amazonaws.com.cn/bucket/the/whole/key');
     t.equal(result.Bucket, 'bucket', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-path dashed in ap-southeast-1', (t) => {
+test('fromUrl: BucketInPath dashed in ap-southeast-1', (t) => {
     const result = s3Urls.fromUrl('https://s3-ap-southeast-1.amazonaws.com/bucket/the/whole/key');
     t.equal(result.Bucket, 'bucket', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-host style', (t) => {
+test('fromUrl: BucketInHost style', (t) => {
     const result = s3Urls.fromUrl('https://bucket.s3.amazonaws.com/the/whole/key');
     t.equal(result.Bucket, 'bucket', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-host style in cn-north-1 w/ dot', (t) => {
+test('fromUrl: BucketInHost style in cn-north-1 w/ dot', (t) => {
     const result = s3Urls.fromUrl('https://results.openaddresses.io.s3.cn-north-1.amazonaws.com.cn/the/whole/key');
     t.equal(result.Bucket, 'results.openaddresses.io', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-host style in cn-north-1 w/ dot & s3', (t) => {
+test('fromUrl: BucketInHost style in cn-north-1 w/ dot & s3', (t) => {
     const result = s3Urls.fromUrl('https://results.s3llout-to-the-man.io.s3.amazonaws.com/the/whole/key');
     t.equal(result.Bucket, 'results.s3llout-to-the-man.io', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-host style in cn-north-1', (t) => {
+test('fromUrl: BucketInHost style in cn-north-1', (t) => {
     const result = s3Urls.fromUrl('https://bucket.s3.cn-north-1.amazonaws.com.cn/the/whole/key');
     t.equal(result.Bucket, 'bucket', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-host style in us-gov-east', (t) => {
+test('fromUrl: BucketInHost style in us-gov-east', (t) => {
     const result = s3Urls.fromUrl('https://s3-us-gov-east-1.amazonaws.com/test-bucket/the/whole/key');
     t.equal(result.Bucket, 'test-bucket', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-host style in us-gov-east', (t) => {
+test('fromUrl: BucketInHost style in us-gov-east', (t) => {
     const result = s3Urls.fromUrl('https://bucket.s3.us-gov-east-1.amazonaws.com/the/whole/key');
     t.equal(result.Bucket, 'bucket', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-host style in ap-southeast-1', (t) => {
+test('fromUrl: BucketInHost style in ap-southeast-1', (t) => {
     const result = s3Urls.fromUrl('https://bucket.s3.ap-southeast-1.amazonaws.com/the/whole/key');
     t.equal(result.Bucket, 'bucket', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-host dashed in cn-north-1', (t) => {
+test('fromUrl: BucketInHost dashed in cn-north-1', (t) => {
     const result = s3Urls.fromUrl('https://bucket.s3-cn-north-1.amazonaws.com.cn/the/whole/key');
     t.equal(result.Bucket, 'bucket', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
     t.end();
 });
 
-test('fromUrl: bucket-in-host dashed in ap-southeast-1', (t) => {
+test('fromUrl: BucketInHost dashed in ap-southeast-1', (t) => {
     const result = s3Urls.fromUrl('https://bucket.s3-ap-southeast-1.amazonaws.com/the/whole/key');
     t.equal(result.Bucket, 'bucket', 'expected bucket');
     t.equal(result.Key, 'the/whole/key', 'expected key');
-    t.end();
-});
-
-test('convert: in-path to s3', (t) => {
-    const result = s3Urls.convert('https://s3.amazonaws.com/bucket/the/whole/key', 's3');
-    t.equal(result, 's3://bucket/the/whole/key');
-    t.end();
-});
-
-test('convert: tileset templates', (t) => {
-    t.equal(s3Urls.convert('https://s3.amazonaws.com/bucket/{z}/{x}/{y}', 's3'), 's3://bucket/{z}/{x}/{y}');
     t.end();
 });
 
